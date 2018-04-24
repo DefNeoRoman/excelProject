dec3698f74086e585ee5af1ac0ba73a9123e7167
rgaponov <rgaponov@omnicomm.ru>
2018-04-24 21:37:49 +0300

    initial commit

--- src/main/java/ExcelTestClass.java
+++ src/main/java/ExcelTestClass.java
@@ -1,5 +1,41 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Date;
+
+
 public class ExcelTestClass {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        Workbook book = new HSSFWorkbook();
+        Sheet sheet = book.createSheet("Birthdays");
+
+        // Нумерация начинается с нуля
+        Row row = sheet.createRow(0);
+
+        // Мы запишем имя и дату в два столбца
+        // имя будет String, а дата рождения --- Date,
+        // формата dd.mm.yyyy
+        Cell name = row.createCell(0);
+        name.setCellValue("John");
+
+        Cell birthdate = row.createCell(1);
+
+        DataFormat format = book.createDataFormat();
+        CellStyle dateStyle = book.createCellStyle();
+        dateStyle.setDataFormat(format.getFormat("dd.mm.yyyy"));
+        birthdate.setCellStyle(dateStyle);
+
+
+        // Нумерация лет начинается с 1900-го
+        birthdate.setCellValue(new Date(110, 10, 10));
+
+        // Меняем размер столбца
+        sheet.autoSizeColumn(1);
+
+        // Записываем всё в файл
+        book.write(new FileOutputStream("abc.xls"));
+        book.close();
     }
 }
