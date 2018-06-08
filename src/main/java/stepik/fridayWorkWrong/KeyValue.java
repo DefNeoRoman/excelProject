3c2369082ba197d812b6aacc2625f81a642f1b24
DefNeoRoman <d8905611060@yandex.ru>
2018-06-08 10:54:35 +0300

    generics hashcode

---
+++ src/main/java/stepik/fridayWorkWrong/KeyValue.java
@@ -0,0 +1,25 @@
+package stepik.fridayWorkWrong;
+
+import java.util.Objects;
+
+public class KeyValue<K,V> {
+    K first;
+    V second;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        KeyValue<?, ?> keyValue = (KeyValue<?, ?>) o;
+
+        return Objects.equals(first, keyValue.first) &&
+                Objects.equals(second, keyValue.second);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 31 * first.hashCode();
+        result = result + result*second.hashCode();
+        return result;
+    }
+}
---
+++ src/main/java/stepik/fridayWorkWrong/Main.java
@@ -0,0 +1,7 @@
+package stepik.fridayWorkWrong;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
