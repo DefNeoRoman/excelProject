3b20badc9f30ea0ee8756f52e793bcd3b4bb9a45
DefNeoRoman <d8905611060@yandex.ru>
2018-06-08 13:18:51 +0300

    mamarama resolved

--- src/main/java/stepik/unit64/TestMamaRama.java
+++ src/main/java/stepik/unit64/TestMamaRama.java
@@ -7,9 +7,13 @@ import java.util.stream.Collectors;
 
 public class TestMamaRama {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+      //  scanner.useDelimiter("[^\\p{L}\\p{Digit}_]+");
+        String in = scanner.nextLine();
       BinaryOperator<Integer> binaryOperator = (integer, integer2) -> integer+integer2;
         String input = "Мама мыла-мыла раму";
-        List<String> wordsList = Arrays.asList(input.split("[^\\p{L}\\p{Digit}_]+"));
+      //  List<String> wordsList = Arrays.asList(input.split("[^\\p{L}\\p{Digit}_]+"));
+        List<String> wordsList = Arrays.asList(in.split("[^\\p{L}\\p{Digit}_]+"));
         ConcurrentMap<String, Integer> collect = wordsList.stream().map(String::toLowerCase)
                 .collect(Collectors.toConcurrentMap(elem -> elem, elem -> 1,(value1,value2)->value1+value2));
         collect.entrySet().stream().sorted(Collections.reverseOrder((o1, o2) -> {
