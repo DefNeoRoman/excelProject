cdb8d8c217b1e6b821a881b64516569d08b8b6cb
rgaponov <rgaponov@omnicomm.ru>
2018-06-12 12:49:33 +0300

    моя реализация LinkedList

--- src/main/java/myRealizationLists/MyList.java
+++ src/main/java/myRealizationLists/MyList.java
@@ -24,4 +24,6 @@ public interface MyList<E> {
     // размер
     int size();
 
+    void clear();
+
 }
--- src/main/java/myRealizationLists/linkedList/MyLinkList.java
+++ src/main/java/myRealizationLists/linkedList/MyLinkList.java
@@ -3,9 +3,54 @@ package myRealizationLists.linkedList;
 import myRealizationLists.MyList;
 
 public class MyLinkList<E> implements MyList<E> {
+    private int size = 0;
+    //Изначально голова является также и хвостом
+    private Node head = new Node(0, null, null);
+    private Node tail;
+
+    public MyLinkList() {
+        tail = new Node(1, head, null);
+        head.prev = tail;
+
+    }
+
     @Override
-    public E get(int i) {
-        return null;
+    public E get(int index) {
+        int currentSize = size();
+        int middle = currentSize / 2;
+        Node firstNode;
+        Node searchNode = new Node(null, null, null);
+        Node secondNode;
+        if (index > middle) {
+            firstNode = tail;
+            secondNode = tail.prev;
+            for (int i = currentSize; i >= 0; i--) {
+                if (i > index) {
+                    firstNode = secondNode;
+                    secondNode = firstNode.prev;
+                }
+                if (i == index) {
+                    searchNode = firstNode;
+
+                }
+
+            }
+        } else {
+            firstNode = head;
+            secondNode = head.prev;
+            for (int i = 0; i <= currentSize; i++) {
+                if (i < index) {
+                    firstNode = secondNode;
+                    secondNode = firstNode.next;
+                }
+                if (i == index) {
+                    searchNode = firstNode;
+                    return (E)searchNode.value;
+                }
+            }
+        }
+
+        return (E) searchNode.value;
     }
 
     @Override
@@ -25,12 +70,51 @@ public class MyLinkList<E> implements MyList<E> {
 
     @Override
     public boolean add(E e) {
-        return false;
+        size++;
+        Node currentNode = tail;
+        tail = new Node(e, null, currentNode);
+        return true;
     }
 
     @Override
     public boolean add(int index, E e) {
-        return false;
+        int currentSize = size();
+        int middle = currentSize / 2;
+        Node firstNode;
+        Node insertNode;
+        Node secondNode;
+        if (index > middle) {
+            firstNode = tail;
+            secondNode = tail.prev;
+            for (int i = currentSize; i >= 0; i--) {
+                if (i > index) {
+                    firstNode = secondNode;
+                    secondNode = firstNode.prev;
+                }
+                if (i == index) {
+                    insertNode = new Node(e, firstNode, secondNode);
+                    firstNode.prev = insertNode;
+                    secondNode.next = insertNode;
+                }
+
+            }
+        } else {
+            firstNode = head;
+            secondNode = head.next;
+            for (int i = 0; i <= currentSize; i++) {
+                if (i < index) {
+                    firstNode = secondNode;
+                    secondNode = firstNode.next;
+                }
+                if (i == index) {
+                    insertNode = new Node(e, secondNode, firstNode);
+                    secondNode.prev = insertNode;
+                    firstNode.next = insertNode;
+                }
+            }
+        }
+        size++;
+        return true;
     }
 
     @Override
@@ -55,6 +139,23 @@ public class MyLinkList<E> implements MyList<E> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    class Node<E> {
+        private E value;
+        private Node next;
+        private Node prev;
+
+        public Node(E value, Node next, Node prev) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 }
